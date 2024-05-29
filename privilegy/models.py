d5ffc49cd38f82f6e3740237564d5f1ece9f429f
amigo657 <amigo.bro.657@gmail.com>
2024-05-30 02:41:03 +0300

    привилегия работает

--- privilegy/models.py
+++ privilegy/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Privilege(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- privilegy/urls.py
+++ privilegy/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import privilegy
+from .views import privilegy, buy_privilege
 
 urlpatterns = [
     path('', privilegy, name = "privilegy_page"),
+    path('buy/<int:privilege_id>/', buy_privilege, name='buy_privilege')
 ]
