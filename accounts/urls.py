42f743404a0a725d9bcdb8569b0341331b17e9d2
amigo657 <amigo.bro.657@gmail.com>
2024-05-30 03:12:56 +0300

    Профиль работает

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('register_user/', register_user, name='register_page'),
     path('login_user/', login_user, name='login_page'),
     path('logout/', log_out, name='log_out_page'),
-    path('profile/', profile, name='profile'),
+    path('profile/', profile, name='profile_page'),
     path('register_hr/', recruiter_register, name='register_page_hr'),
     path('login_hr/', recruiter_login, name='login_page_hr'),
 ]
--- privilegy/models.py
+++ privilegy/models.py
@@ -4,7 +4,7 @@ class Privilege(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    time = models.IntegerField()
+    time = models.DurationField()
 
     def __str__(self):
         return self.title
